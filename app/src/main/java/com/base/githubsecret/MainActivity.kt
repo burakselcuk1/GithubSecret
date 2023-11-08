f093824dde5def4278ac9053eb813181ae22aa74
burakselcuk1 <design.burakk@gmail.com>
2023-11-08 14:00:57 +0300

    Initial commit

--- app/src/main/java/com/base/githubsecret/MainActivity.kt
+++ app/src/main/java/com/base/githubsecret/MainActivity.kt
@@ -1,37 +1,63 @@
 package com.base.githubsecret
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     private var BASE_URL = "Buraya Github Actions ile alınan BASE_URL gelecek"
+    private var runId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       var baseUrlTextView:TextView = findViewById<TextView>(R.id.basee)
+        val button: Button = findViewById<Button>(R.id.button)
+        val baseUrlTextView: TextView = findViewById<TextView>(R.id.basee)
 
-        // GitHub Actions tarafından oluşturulan dosyayı okuyun
-        try {
-            val file = File(filesDir, "base_url.txt")
-            val bufferedReader = BufferedReader(FileReader(file))
-            val stringBuilder = StringBuilder()
-            var line: String?
-            while (bufferedReader.readLine().also { line = it } != null) {
-                stringBuilder.append(line)
+        button.setOnClickListener {
+            // Arka plan iş parçacığında ağ isteği yap
+            GlobalScope.launch(Dispatchers.IO) {
+                // "run_id" değerini al
+                runId = getRunId()
+                val base_url = getBaseUrl()
+                runOnUiThread {
+                    baseUrlTextView.text = base_url
+                }
             }
-            BASE_URL = stringBuilder.toString()
-            bufferedReader.close()
+        }
+    }
+
+    private fun getBaseUrl(): String {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.github.com/repos/burakselcuk1/GithubSecret/actions/artifacts/runs/$runId/artifacts/base_url")
+            .get()
+            .build()
+
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                return response.body()?.string() ?: "Buraya Github Actions ile alınan BASE_URL gelecek"
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        baseUrlTextView.text = BASE_URL
+        return "Ağ isteği başarısız oldu."
+    }
+
+    private fun getRunId(): String? {
+        // "github.run_id" değerini döndürerek "run_id" değerini alabilirsiniz
+        return "github.run_id" // Gerçek değeri bu şekilde almalısınız
     }
 }
+
